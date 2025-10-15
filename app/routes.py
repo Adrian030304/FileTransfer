04b39a4f32aa0696aaee18fa6500c8549431193e
Adrian030304 <adrianromanenco1@gmail.com>
2025-10-15 22:28:58 +0300

    header and templates implement

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
-
+from flask import render_template, redirect
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
