0b4dce7461abfd39853d3088d0089aa007e06a7a
Adrian030304 <adrianromanenco1@gmail.com>
2025-10-16 16:52:22 +0300

    recreate routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from app import app
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for
 
 @app.route('/')
-@app.route('/index')
+def home():
+    return redirect(url_for('index'))
+
+@app.route('/main-page')
 def index():
-    return render_template('index.html')
+    return render_template('main_page.html')
