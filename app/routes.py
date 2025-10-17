d440aa4183abaecf0b5805bae743a511f59ca634
Adrian030304 <adrianromanenco1@gmail.com>
2025-10-17 04:04:10 +0300

    imported zip and temp file to create a temporary zip when multiple files are chosen, fleshed out the logic for upload route and implemented the logic for downloading by having conditions for downloading multiple files and putting them in a zip and when downloading only one file to just download it standalone.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, send_from_directory, send_file
 from werkzeug.utils import secure_filename
 from .forms import FileForm
 from .utils import generate_random_string
-import os
+from zipfile import ZipFile, ZIP_DEFLATED
+import os, tempfile
+
+upload_location = app.config['UPLOAD_FOLDER']
 
 @app.route('/')
 def home():
@@ -15,21 +18,56 @@ def index():
 
 @app.route('/transfer-page', methods=['POST','GET'])
 def transfer():
-    upload_location = app.config['UPLOAD_FOLDER']
+    
     os.makedirs(upload_location, exist_ok=True)
     
+    access_code = None
+
     form = FileForm()
+    saving_folder = None
     if request.method == 'POST':
         if form.validate_on_submit():
-            folder_name = str(generate_random_string())
-            os.makedirs(os.path.join(upload_location, folder_name),exist_ok=True)
+            saving_folder = os.path.join(upload_location, str(generate_random_string()))
+            os.makedirs(saving_folder,exist_ok=True)
             
+            if saving_folder and os.path.exists(saving_folder):
+                access_code = saving_folder.rsplit('\\', 1)[-1]
+
             for file in form.files.data:
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(upload_location, folder_name, filename))
-            return redirect()
-    return render_template('upload_page.html', form=form)
+                file.save(os.path.join(saving_folder, filename))
+        
+        
+    return render_template('upload_page.html', form=form, access_code=access_code)
 
-@app.route('/download-file')
-def download_file():
-    pass
+@app.route('/download-file/<download_code>', methods=['POST', 'GET'])
+def download_file(download_code):
+    download_directory = os.path.join(upload_location, download_code)
+    files_list = os.listdir(download_directory)
+    # print(os.path.join(download_directory, files_list[0]))
+
+    if len(files_list) > 1:
+        # creez fisier temporar
+        temp_zip = tempfile.NamedTemporaryFile(delete=False, suffix='.zip')
+        temp_zip.close()
+    
+        with ZipFile(temp_zip.name, 'w', ZIP_DEFLATED) as fisier_zip:
+            for fisier in files_list:
+                filename = os.path.join(download_directory, fisier)
+                fisier_zip.write(filename, fisier) # va fi scris doar numele fisierului in zip fara sa ii apara path ul
+        response = send_file(temp_zip.name, mimetype='application/zip', as_attachment=True, download_name=download_code[:6])
+
+        @response.call_on_close
+        def delete_zip():
+            os.remove(temp_zip.name)
+
+        return response
+
+    else:
+        try:
+            return send_from_directory(download_directory,files_list[0], as_attachment=True)
+        except FileNotFoundError:
+            return "File not found", 404
+    
+    
+    return render_template('download.html', files = files_list )
