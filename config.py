d8af6e3cb3a617b5a72d3909d70c1d2e09172963
Adrian030304 <adrianromanenco1@gmail.com>
2025-11-08 22:09:22 +0200

    initialize database

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app', 'static' , 'uploads')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    
